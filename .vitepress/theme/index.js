8e86d87fb8b5e56a933fe9505f7ab67c2e71341b
吴磊 <wulei0407@163.com>
2024-11-28 15:36:06 +0800

    更新：添加YouTubeVideo组件

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,5 +1,11 @@
 // .vitepress/theme/index.js
 import DefaultTheme from "vitepress/theme"
 import "./custom.css"
+import YouTubeVideo from "./components/YouTubeVideo.vue"
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  enhanceApp({ app }) {
+    app.component("YouTubeVideo", YouTubeVideo)
+  },
+}
