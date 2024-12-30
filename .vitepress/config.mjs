5ffd66bddb9b3cd10e4ea5db9b0cafad946c7219
吴磊 <wulei0407@163.com>
2024-12-30 14:56:02 +0800

    更新：更新文档

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
       { text: "首页", link: "/" },
       { text: "教程简介", link: "/intro.md" },
       { text: "核心功能", link: "/core/脚手架核心.md" },
-      { text: "更新日志", link: "/log/2024-12-24.md" },
+      { text: "更新日志", link: "/log/2024-12-30.md" },
     ],
     search: {
       provider: "local",
@@ -41,6 +41,14 @@ export default defineConfig({
         {
           text: "更新日志",
           items: [
+            {
+              text: "重构Eslint配置",
+              link: "/log/2024-12-30.md",
+            },
+            {
+              text: "使用simple-git-hooks",
+              link: "/log/2024-12-29.md",
+            },
             {
               text: "Auto Decimal插件",
               link: "/log/2024-12-24.md",
